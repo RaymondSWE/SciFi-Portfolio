e187c0613efda9c012867354cf0eacaaa469a5fd
RaymondSWE <RamanMohammed@hotmail.com>
2024-02-28 22:14:46 +0100

    new header in herocontent

--- components/main/HeroContent.tsx
+++ components/main/HeroContent.tsx
@@ -33,12 +33,12 @@ const HeroContent = () => {
             className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
           >
             <span>
-              Crafting
+              Building
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
                 {" "}
-                innovative{" "}
+                 cool stuff{" "}
               </span>
-              software solutions
+              with code
             </span>
           </motion.div>
 
