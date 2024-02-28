0834a6c0af71628eb49d08a2b547c32a0558aa3f
RaymondSWE <RamanMohammed@hotmail.com>
2024-02-28 22:03:25 +0100

    update wrong href in navbar

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
-          href="#about-me"
+          href="#hero"
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
