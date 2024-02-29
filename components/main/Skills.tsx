6a48ae6a00fb754bee00927f690e9e8fa021afb1
RaymondSWE <RamanMohammed@hotmail.com>
2024-02-29 21:04:18 +0100

    remoed skillstext from ui folder

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Backend_skill,
   Frontend_skill,
@@ -5,9 +6,13 @@ import {
   Other_skill,
 } from "@/data/Skills";
 import React from "react";
-import SkillDataProvider from "../ui/SkillDataProvider";
-import SkillText from "../ui/SkillText";
 import SkillSection from "../ui/SkillSection";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
 
 const Skills = () => {
   return (
@@ -16,7 +21,14 @@ const Skills = () => {
       className=" flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
       style={{ transform: "scale(0.9" }}
     >
-      <SkillText />
+      <div className="w-full h-auto flex flex-col items-center justify-center">
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
+        >
+          Making apps with modern technologies
+        </motion.div>
+      </div>
       <SkillSection skillsData={Frontend_skill} />
       <SkillSection skillsData={Backend_skill} />
       <SkillSection skillsData={Devops} />
