6e4b47745b9464632c2b1e4a91a51077402d13eb
RaymondSWE <RamanMohammed@hotmail.com>
2024-03-05 19:32:32 +0100

    added more certificaiton data

--- data/Certification.ts
+++ data/Certification.ts
@@ -2,7 +2,7 @@ export const CertificationData = [
     {
       id: 1,
       name: "Docker Mastery: with Kubernetes + Docker Swarm from a Docker Captain",
-      date: "Ongoing",
+      date: "01/23/2024",
       description:
         "Comprehensive guide to using Docker, Compose, and Kubernetes. Includes CI/CD examples, configuration best practices, orchestration tool management, container creation, image registry, and workflow optimization.",
         CertificationLink: "https://www.udemy.com/certificate/UC-1fc30466-eee4-488e-9993-67d336a8c927/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com",
@@ -33,6 +33,40 @@ export const CertificationData = [
         light: true 
 
     },
+    {
+      "id": 4,
+      "name": "CCNAv7: Introduction to Networks",
+      "date": "19/02/2024",
+      "description": "Proficient in configuring switches, routers, and troubleshooting connectivity for network security and resource access.",
+      "CertificationLink": "https://www.linkedin.com/in/raman-mohammed/overlay/1708435218571/single-media-viewer/?profileId=ACoAADT0IfMBLEd3A48vbUCNtPekJiE7dxDF4EI",
+      "backgroundImg": "/card-background/card-bg-6.jpg",
+      "borderBackground": "/card-border/card-4.svg",
+      "light": true
+    },
+    
+    {
+      id: 5,
+      name: "Microsoft Azure: From Zero to Hero",
+      date: "Ongoing",
+      description:
+        "This certification, will transition from basic cloud concepts to mastering advanced Azure services and architectural designs. ",
+        CertificationLink: "https://ramanmohammedportfolio.mel",
+        backgroundImg: "/card-background/card-bg-7.jpg",
+        borderBackground: "/card-border/card-5.svg",
+        light: true 
+
+    },
+    {
+      id: 6,
+      name: "Spring Boot 3, Spring 6 & Hibernate for Beginners",
+      date: "Ongoing",
+      description:
+        "Include Spring Boot 3, Spring 6, and Hibernate. Learn core concepts, REST APIs, MVC, Security, JPA, and more with hands-on projects. ",
+        CertificationLink: "https://ramanmohammedportfolio.mel",
+        backgroundImg: "/card-background/card-bg-8.jpg",
+        borderBackground: "/card-border/card-3.svg",
+        light: true 
+
+    },
 ];
 
-// /starwars-bg-1.jpg
