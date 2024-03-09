457bb21bb400a8056ed6cb5ff41ff264434223c6
RaymondSWE <RamanMohammed@hotmail.com>
2024-03-09 19:24:28 +0100

    fixed the data content for service

--- components/main/Services.tsx
+++ components/main/Services.tsx
@@ -1,24 +1,26 @@
 "use client";
 import { ServicesApps, ServicesData } from "@/data/Services";
 import React from "react";
-import { LeftCurve, RightCurve } from "../ui/Curves";
+import { LeftCurve, RightCurve } from "../ui/ServiceCurves";
 import Image from "next/image";
 import SectionHeader from "../ui/SectionHeader";
-import { GlobeAltIcon } from "@heroicons/react/24/solid";
+import { ShoppingCartIcon  } from "@heroicons/react/24/solid";
 import ServiceItem from "../ui/ServiceItem";
 import ServiceIconItem from "../ui/ServiceIconItem";
 
 const Services = () => {
+
+    const totalCount = ServicesApps.length;
+
   return (
     <section className="px-4 lg:px-8 xl:px-16">
       <SectionHeader
-        title="Nebula of Innovation"
-        subtitle="Where ideas collide and form stars of innovation"
-        Icon={<GlobeAltIcon />}
+        title="Galactic Development Services"
+        subtitle="Engineering your software products from Coruscant to the Outer Rim."
+        Icon={<ShoppingCartIcon  />}
       />{" "}
       <div className="container lg:flex mx-auto">
         <div className="max-w-[25rem] lg:w-1/2">
-          <h2 className="h2 mb-4 md:mb-8 text-white">Somekinda Services</h2>
           <ul className="max-w-[22rem] mb-10 md:mb-14">
             {ServicesData.map((service) => (
               <ServiceItem
@@ -30,12 +32,11 @@ const Services = () => {
               />
             ))}
           </ul>
-          <button className="white">idk</button>
         </div>
 
         <div className="lg:ml-auto xl:w-[38rem] lg:w-1/2 mt-4">
           <p className="mb-4 text-gray-500 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
-            SERVICE TEXT
+          Discover my array of provided services, crafted with precision and passion. My portfolio showcases my expertise in solving diverse technological challenges, from sleek web designs to robust backend systems.
           </p>
           <div className="relative left-1/2 flex w-[22rem] aspect-square border border-gray-700 rounded-full -translate-x-1/2 scale-75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-gray-700 rounded-full">
@@ -59,6 +60,7 @@ const Services = () => {
                   icon={app.icon}
                   width={app.width}
                   height={app.height}
+                  totalCount={totalCount}
                 />
               ))}
             </ul>
--- components/ui/ServiceIconItem.tsx
+++ components/ui/ServiceIconItem.tsx
@@ -7,6 +7,8 @@ interface ServiceIconItemProps {
   icon: string;
   width: number;
   height: number;
+  totalCount: number;
+
 }
 
 const ServiceIconItem = ({
@@ -15,16 +17,18 @@ const ServiceIconItem = ({
   icon,
   width,
   height,
+  totalCount
 }: ServiceIconItemProps) => {
+    const rotationDeg = 360 / totalCount;
   return (
     <li
       key={id}
       className="absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom"
-      style={{ transform: `rotate(${parseInt(id) * 45}deg)` }}
+      style={{ transform: `rotate(${parseInt(id) * rotationDeg}deg)` }}
     >
       <div
         className="relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-gray-700 border border-n-1/15 rounded-xl"
-        style={{ transform: `rotate(-${parseInt(id) * 45}deg)` }}
+        style={{ transform: `rotate(-${parseInt(id) * rotationDeg}deg)` }}
       >
         <Image
           className="m-auto"
