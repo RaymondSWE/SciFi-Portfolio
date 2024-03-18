ee787efa6a06fd9deabc5577db53d4cb1b7c76bc
RaymondSWE <RamanMohammed@hotmail.com>
2024-03-18 05:58:56 +0100

    tooltip added

--- components/ui/ServiceIconItem.tsx
+++ components/ui/ServiceIconItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Tooltip from "./Tooltip";
 
 interface ServiceIconItemProps {
   id: string;
@@ -25,6 +26,7 @@ const ServiceIconItem = ({
       className="absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom"
       style={{ transform: `rotate(${parseInt(id) * rotationDeg}deg)` }}
     >
+      <Tooltip content={title} position="top">
       <div
         className="relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-gray-700 border border-gray-400	 rounded-xl"
         style={{ transform: `rotate(-${parseInt(id) * rotationDeg}deg)` }}
@@ -37,6 +39,7 @@ const ServiceIconItem = ({
           src={icon}
         />
       </div>
+      </Tooltip>
     </li>
   );
 };
---
+++ components/ui/Tooltip.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+interface TooltipProps {
+  content: String; // The content of the tooltip
+  children: React.ReactElement; // The element that will trigger the tooltip
+  position?: 'top' | 'right' | 'bottom' | 'left';
+}
+
+const Tooltip = ({ content, children, position = 'top' }: TooltipProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showTooltip = () => setIsVisible(true);
+  const hideTooltip = () => setIsVisible(false);
+
+  return (
+    <div className="relative flex items-center justify-center">
+      {isVisible && (
+        <div
+          className={`absolute z-10 p-2 text-sm text-gray-300 bg-black rounded-md ${positionClasses[position]}`}
+          role='tooltip'
+          aria-hidden={!isVisible}
+        >
+          {content}
+        </div>
+      )}
+      <div onMouseEnter={showTooltip} onMouseLeave={hideTooltip} aria-haspopup="true">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+const positionClasses = {
+    top: 'bottom-full mb-8',
+    right: 'left-full ml-8',
+    bottom: 'top-full mt-2',
+    left: 'right-full mr-8',
+  };
+
+export default Tooltip;
