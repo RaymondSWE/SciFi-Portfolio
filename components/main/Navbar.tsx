84e49895cc7c96e5ba37d919d0201c544535e83a
RaymondSWE <RamanMohammed@hotmail.com>
2024-03-31 00:34:50 +0100

    fixed navbar mobile responsive of navlinks

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -21,12 +21,11 @@ const socialLinks = [
     label: "LinkedIn",
     icon: <FaLinkedinIn />,
   },
-  
 ];
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-row shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] border-b-0 border-[#7042f861] w-full py-4 px-12">
+    <nav className="flex flex-row shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] border-b-0 border-[#7042f861] w-full py-4">
       <Image
         src="/main.svg"
         alt="background gradient image"
@@ -34,13 +33,13 @@ const Navbar = () => {
         className="z-[-10] object-cover opacity-80"
       />
       <div className="flex justify-between w-full px-8">
-        <div className="md:flex items-center hidden">
+        <div className="md:flex items-center hidden  ml-12">
           <FaRocket className="text-purple-500 text-2xl mr-2" />
-          <span className="font-bold md:text-2xl text-xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500 cursive">
+          <span className="font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-blue-500 cursive">
             Raman Mohammed
           </span>
         </div>
-        <div className="flex justify-between w-full bg-[#03001442] border border-[#7042f861] backdrop-blur-xl rounded-full text-gray-200 px-4 py-4 space-x-2 z-50 flex-grow md:max-w-xl">
+        <div className="flex justify-between w-full bg-[#03001442] border border-[#7042f861] backdrop-blur-xl rounded-full text-gray-200 px-2 md:px-4 py-4 space-x-2 z-50 flex-grow md:max-w-xl">
           {navLinks.map((link) => (
             <a
               key={link.href}
@@ -51,12 +50,12 @@ const Navbar = () => {
             </a>
           ))}
         </div>
-        <div className="hidden md:flex gap-6 ml-6 z-50 justify-end items-center">
+        <div className="hidden md:flex gap-6 ml-6 z-50 justify-end items-center mr-12">
           {socialLinks.map((link) => (
             <a
               key={link.href}
               href={link.href}
-              className="text-white text-2xl md:text-4xl hover:scale-110 transition-transform"
+              className="text-white text-4xl hover:scale-110 transition-transform"
               target="_blank"
               rel="noreferrer"
             >
