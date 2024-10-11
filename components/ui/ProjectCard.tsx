182f5862ce000e5e4ff9cea731d6c2c12ed744a8
RaymondSWE <RamanMohammed@hotmail.com>
2024-10-12 00:00:28 +0200

    Refactor: Update max-w-xs in ProjectCard component

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -45,14 +45,13 @@ const ProjectCard = ({
     <>
       <motion.div
         data-attribute-id={id}
-        className="group z-10 flex flex-col overflow-hidden rounded-lg bg-[#030014] border border-[#24282c] hover:scale-110 hover:shadow-lg  max-w-xs mx-auto relative"
+        className="group z-10 flex flex-col overflow-hidden rounded-lg bg-[#030014] border border-[#24282c] hover:scale-110 hover:shadow-lg  max-w-[350px] mx-auto relative"
         ref={ref}
         initial="hidden"
         animate={inView ? 'visible' : 'hidden'}
         variants={slideInFromTop(0.5)}
         aria-labelledby={`project-title-${id}`}
         tabIndex={0}
-        
       >
         
         <div className="w-full h-48 relative p-2">
@@ -82,84 +81,80 @@ const ProjectCard = ({
             {description}
           </p>
         </div>
-
-        <div className="z-20 flex justify-around items-center p-4 bg-gradient-to-b from-[#3c087e1f] to-[#3c087e6b] shadow-inner">
+        <div className="z-20 flex justify-around py-4 bg-gradient-to-b from-[#3c087e1f] to-[#3c087e6b] shadow-inner">
         {githubUrl ? (
-            <a
-              href={githubUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={`View ${title} on GitHub`}
-              className="flex flex-col items-center text-white z-20"
-              onClick={(e) => e.stopPropagation()} 
-            >
-              <FaGithub className="text-2xl" />
-              <span className="text-xs mt-1">Github Link</span>
-            </a>
-          ) : (
-            <div
-              className="flex flex-col items-center text-white opacity-50 cursor-not-allowed"
-              onClick={() => handleMissingLink('Github')}
-              aria-hidden="true"
-            >
-              <FaGithub className="text-2xl" />
-              <span className="text-xs mt-1">N/A</span>
-            </div>
-          )}
+          <a
+            href={githubUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={`View ${title} on GitHub`}
+            className="flex items-center text-white z-20 space-x-2"
+            onClick={(e) => e.stopPropagation()} 
+          >
+            <FaGithub className="text-xl" />
+            <span className="text-xs mt-0">Github Link</span>
+          </a>
+        ) : (
+          <div
+            className="flex items-center text-white opacity-50 cursor-not-allowed space-x-2"
+            onClick={() => handleMissingLink('Github')}
+            aria-hidden="true"
+          >
+            <FaGithub className="text-xl" />
+            <span className="text-xs mt-0">N/A</span>
+          </div>
+        )}
 
-          {sourceUrl ? (
-            <a
-              href={sourceUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={`View ${title} live demo`}
-              className="flex flex-col items-center text-white"
-              onClick={(e) => e.stopPropagation()} 
-            >
-              <FaExternalLinkAlt className="text-2xl" />
-              <span className="text-xs mt-1">Live Demo</span>
-            </a>
-          ) : (
-            <div
-              className="flex flex-col items-center text-white opacity-50 cursor-not-allowed"
-              onClick={() => handleMissingLink('live demo')}
-              aria-hidden="true"
-            >
-              <FaExternalLinkAlt className="text-2xl" />
-              <span className="text-xs mt-1">
-                N/A
-              </span>
-            </div>
-          )}
+        {sourceUrl ? (
+          <a
+            href={sourceUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={`View ${title} live demo`}
+            className="flex items-center text-white space-x-2"
+            onClick={(e) => e.stopPropagation()} 
+          >
+            <FaExternalLinkAlt className="text-xl" />
+            <span className="text-xs mt-0">Demo</span>
+          </a>
+        ) : (
+          <div
+            className="flex items-center text-white opacity-50 cursor-not-allowed space-x-2"
+            onClick={() => handleMissingLink('live demo')}
+            aria-hidden="true"
+          >
+            <FaExternalLinkAlt className="text-xl" />
+            <span className="text-xs mt-0">N/A</span>
+          </div>
+        )}
 
-          {youtubeUrl ? (
-            <a
-              href={youtubeUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label={`Watch ${title} on YouTube`}
-              className="flex flex-col items-center text-white"
-              onClick={(e) => e.stopPropagation()} 
-            >
-              <FaYoutube className="text-2xl" />
-              <span className="text-xs mt-1">Youtube Link</span>
-            </a>
-          ) : (
-            <div
-              className="flex flex-col items-center text-white opacity-50 cursor-not-allowed"
-              onClick={() => handleMissingLink('Youtube')}
-              aria-hidden="true"
-            >
-              <FaYoutube className="text-2xl" />
-              <span className="text-xs mt-1">
-                N/A
-              </span>
-            </div>
-          )}
-        </div>
+        {youtubeUrl ? (
+          <a
+            href={youtubeUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={`Watch ${title} on YouTube`}
+            className="flex items-center text-white space-x-2"
+            onClick={(e) => e.stopPropagation()} 
+          >
+            <FaYoutube className="text-xl" />
+            <span className="text-xs mt-0">Youtube Link</span>
+          </a>
+        ) : (
+          <div
+            className="flex items-center text-white opacity-50 cursor-not-allowed space-x-2"
+            onClick={() => handleMissingLink('Youtube')}
+            aria-hidden="true"
+          >
+            <FaYoutube className="text-xl" />
+            <span className="text-xs mt-0">N/A</span>
+          </div>
+        )}
+      </div>
+
 
         <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        <span className="text-white bg-black bg-opacity-50 rounded-lg px-4 py-2">
+        <span className="text-white bg-black bg-opacity-50 rounded-lg px-4 py-2 z-20">
         Click the image to view details
         </span>
       </div>
