464910ee587b6229e295a04b2aae1718f50cb462
RaymondSWE <RamanMohammed@hotmail.com>
2024-10-14 13:31:37 +0200

    Refactor: Update ProjectCard component to adjust the position of the details tooltip and add an animated "In Progress" badge

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -64,12 +64,11 @@ const ProjectCard = ({
           />
           <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-black via-transparent opacity-75 rounded-b-lg" />
           {endDate === null && (
-            <div className="absolute top-2 left-2 bg-gradient-to-r from-purple-500 to-indigo-500 shadow-md text-white text-xs font-bold py-1 px-2 rounded">
-              In Progress
-            </div>
-          )}
+          <div className="z-30 absolute top-2 left-2 bg-gradient-to-b from-[#3c087e1f] to-[#3c087e6b] shadow-inner text-white text-sm font-extrabold py-1 px-3 rounded-3xl flex items-center space-x-2 animate-pulse">
+            <span>In Progress</span>
+          </div>
+        )}
         </div>
-
         <div className="flex-grow p-4">
           <h1
             className="text-lg font-bold text-center text-white"
@@ -153,7 +152,7 @@ const ProjectCard = ({
       </div>
 
 
-        <div className="absolute inset-0 flex items-start justify-center mt-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div className="absolute inset-0 flex items-start justify-center mt-12 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         <span className="text-white bg-black bg-opacity-50 rounded-lg px-4 py-2 z-20">
         Click the image to view details
         </span>
