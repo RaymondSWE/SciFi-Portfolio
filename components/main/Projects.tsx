b5061d0e74bedcbee1b7789ad84f9cc7dfa3508f
RaymondSWE <RamanMohammed@hotmail.com>
2024-10-22 18:55:26 +0200

    Refactor: Adjust grid layout in Projects component

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -44,7 +44,7 @@ const Projects = () => {
         {projectsCategories.map((category) => (
           <TabsContent key={category} value={category}>
             <div
-              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-6 md:px-12"
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-6 md:px-12"
               aria-label={`${category} project list`}
             >
               {filterProjectsByCategory(category).map((project) => (
