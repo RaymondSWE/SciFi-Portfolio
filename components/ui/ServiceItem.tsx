ac31ec456e4b32a08fd9cdd59f99f38d27db8983
RaymondSWE <RamanMohammed@hotmail.com>
2024-11-21 02:38:28 +0100

    Refactor: Adjust icon size and margin in ServiceItem component for better layout

--- components/ui/ServiceItem.tsx
+++ components/ui/ServiceItem.tsx
@@ -15,14 +15,14 @@ const ServiceItem = ({ id, name, description, icon }: ServiceData) => {
   const { ref, inView } = useInView({ triggerOnce: true });
   return (
     <motion.div
-      className="flex flex-col  mb-10 md:mb-14 bg-[rgba(33,149,243,0.07)] rounded-lg shadow-lg shadow-blue-700/20 p-4 backdrop-blur-sm"
+      className="flex flex-col mb-10 md:mb-14 bg-[rgba(33,149,243,0.07)] rounded-lg shadow-lg shadow-blue-700/20 p-4 backdrop-blur-sm"
       ref={ref}
       initial="hidden"
       animate={inView ? 'visible' : 'hidden'}
       variants={slideInFromTop(0.2)}
     >
         <div className="flex">
-          <Image className='mr-4 ' src={icon} width={24} height={24} alt={`Icon representing ${name}`} />
+          <Image className='mr-2' src={icon} width={20} height={20} alt={`Icon representing ${name}`} />
           <h6 className="Welcome-text text-base md:text-lg font-bold">
             {name}
           </h6>
